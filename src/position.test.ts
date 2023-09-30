fb3552cb068ee12feb8cc10c989e2475f668d678
Vikas Gorur <vikas@airbase.io>
2023-09-30 12:22:46 +0530

    Compute material

--- src/position.test.ts
+++ src/position.test.ts
@@ -1,10 +1,20 @@
-import { Chess } from 'chess.js'
-import { occupiedSquares, Square } from './position'
+import { Chess, Color } from 'chess.js'
+import { occupiedSquares, material } from './position'
 
 describe('occupiedSquares', () => {
     it('should return the correct number of squares', () => {
         let b = new Chess()
-        let squares = occupiedSquares(b, 'w')
-        expect(squares.length).toBe(16)
+        expect(occupiedSquares(b, 'w').length).toBe(16)
+    })
+})
+
+describe('material', () => {
+    it('should return the correct material', () => {
+        let b = new Chess()
+        expect(material(b, 'w')).toBe(
+            8       // pawns
+            + 4*3   // knights & bishops
+            + 2*5   // rooks
+            + 9)    // queen
     })
 })
--- src/position.ts
+++ src/position.ts
@@ -7,12 +7,31 @@ export interface Square {
     color: Color,
 }
 
+type PieceValues = { [key in PieceSymbol]: number }
+
+const PIECE_VALUES: PieceValues = {
+    p: 1,
+    n: 3,
+    b: 3,
+    r: 5,
+    q: 9,
+    k: 0,
+}
+
 export function occupiedSquares(b: Chess, color: Color): Square[] {
     let result: Square[] = []
     for (let s of _.flatten(b.board())) {
         if (s && s.color === color) {
             result.push(s)
         }
+    }
+    return result
+}
+
+export function material(b: Chess, color: Color): number {
+    let result = 0
+    for (let s of occupiedSquares(b, color)) {
+        result += PIECE_VALUES[s.type]
     }
     return result
 }
