fa626f120d301eafd66f212767d077b529f4b40a
Vikas Gorur <vikas@airbase.io>
2023-09-30 18:28:46 +0530

    Piece attack functions

--- src/position.test.ts
+++ src/position.test.ts
@@ -1,10 +1,14 @@
+import _ from 'lodash'
 import { Chess, Color } from 'chess.js'
-import { occupiedSquares, material } from './position'
+import {
+    places, material, deltaSet,
+    pawnAttacks, knightAttacks, bishopAttacks, rookAttacks, queenAttacks,
+} from './position'
 
 describe('occupiedSquares', () => {
     it('should return the correct number of squares', () => {
         let b = new Chess()
-        expect(occupiedSquares(b, 'w').length).toBe(16)
+        expect(places(b, 'w').length).toBe(16)
     })
 })
 
@@ -16,5 +20,102 @@ describe('material', () => {
             + 4*3   // knights & bishops
             + 2*5   // rooks
             + 9)    // queen
+    })
+})
+
+describe('deltaSet', () => {
+    it('should respect board boundaries', () => {
+        expect(deltaSet('a1', [[-1, -1]])).toEqual([])
+        expect(deltaSet('h8', [[1, 1]])).toEqual([])
+        expect(deltaSet('a8', [[-1, 1]])).toEqual([])
+        expect(deltaSet('h1', [[1, -1]])).toEqual([])
+        expect(deltaSet('b8', [[0, 1]])).toEqual([])
+    })
+})
+
+describe('pawnAttacks', () => {
+    it('should return the correct squares', () => {
+        expect(pawnAttacks('a2', 'w')).toEqual(['b3'])
+        expect(pawnAttacks('h2', 'w')).toEqual(['g3'])
+        expect(pawnAttacks('a7', 'b')).toEqual(['b6'])
+        expect(pawnAttacks('h7', 'b')).toEqual(['g6'])
+        expect(pawnAttacks('e4', 'w')).toEqual(['d5', 'f5'])
+    })
+})
+
+describe('knightAttacks', () => {
+    it('should return the correct squares', () => {
+        expect(_.sortBy(knightAttacks('a1'))).toEqual(_.sortBy(['b3', 'c2']))
+        expect(_.sortBy(knightAttacks('h1'))).toEqual(_.sortBy(['g3', 'f2']))
+        expect(_.sortBy(knightAttacks('a8'))).toEqual(_.sortBy(['b6', 'c7']))
+        expect(_.sortBy(knightAttacks('h8'))).toEqual(_.sortBy(['g6', 'f7']))
+        expect(_.sortBy(knightAttacks('e4'))).toEqual(_.sortBy(['d6', 'f6', 'c5', 'g5', 'c3', 'g3', 'd2', 'f2']))
+    })
+})
+
+describe('bishopAttacks', () => {
+    it('should return the correct squares for a1', () => {
+        expect(_.sortBy(bishopAttacks('a1'))).toEqual(_.sortBy(['b2', 'c3', 'd4', 'e5', 'f6', 'g7', 'h8']))
+    })
+
+    it('should return the correct squares for d4', () => {
+        expect(_.sortBy(bishopAttacks('d4'))).toEqual(_.sortBy([
+            'a1', 'b2', 'c3', 'e5', 'f6', 'g7', 'h8',
+            'a7', 'b6', 'c5', 'e3', 'f2', 'g1',
+        ]))
+    })
+
+    it('should return the correct squares for h8', () => {
+        expect(_.sortBy(bishopAttacks('h8'))).toEqual(_.sortBy(['a1', 'b2', 'c3', 'd4', 'e5', 'f6', 'g7']))
+    })
+})
+
+describe('rookAttacks', () => {
+    it('should return the correct squares for a1', () => {
+        expect(_.sortBy(rookAttacks('a1'))).toEqual(_.sortBy([
+            'a2', 'a3', 'a4', 'a5', 'a6', 'a7', 'a8',
+            'b1', 'c1', 'd1', 'e1', 'f1', 'g1', 'h1',
+        ]))
+    })
+
+    it('should return the correct squares for d4', () => {
+        expect(_.sortBy(rookAttacks('d4'))).toEqual(_.sortBy([
+            'a4', 'b4', 'c4', 'e4', 'f4', 'g4', 'h4',
+            'd1', 'd2', 'd3', 'd5', 'd6', 'd7', 'd8',
+        ]))
+    })
+
+    it('should return the correct squares for h8', () => {
+        expect(_.sortBy(rookAttacks('h8'))).toEqual(_.sortBy([
+            'a8', 'b8', 'c8', 'd8', 'e8', 'f8', 'g8',
+            'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'h7',
+        ]))
+    })
+})
+
+describe('queenAttacks', () => {
+    it('should return the correct squares for a1', () => {
+        expect(_.sortBy(queenAttacks('a1'))).toEqual(_.sortBy([
+            'a2', 'a3', 'a4', 'a5', 'a6', 'a7', 'a8',
+            'b2', 'c3', 'd4', 'e5', 'f6', 'g7', 'h8',
+            'b1', 'c1', 'd1', 'e1', 'f1', 'g1', 'h1',
+        ]))
+    })
+
+    it('should return the correct squares for d4', () => {
+        expect(_.sortBy(queenAttacks('d4'))).toEqual(_.sortBy([
+            'a4', 'b4', 'c4', 'e4', 'f4', 'g4', 'h4',
+            'd1', 'd2', 'd3', 'd5', 'd6', 'd7', 'd8',
+            'a1', 'b2', 'c3', 'e5', 'f6', 'g7', 'h8',
+            'a7', 'b6', 'c5', 'e3', 'f2', 'g1',
+        ]))
+    })
+
+    it('should return the correct squares for h8', () => {
+        expect(_.sortBy(queenAttacks('h8'))).toEqual(_.sortBy([
+            'a8', 'b8', 'c8', 'd8', 'e8', 'f8', 'g8',
+            'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'h7',
+            'g7', 'f6', 'e5', 'd4', 'c3', 'b2', 'a1',
+        ]))
     })
 })
--- src/position.ts
+++ src/position.ts
@@ -1,8 +1,12 @@
 import _ from 'lodash'
-import { Chess, PieceSymbol, Color } from 'chess.js'
+import { Chess, PieceSymbol, Color, Square } from 'chess.js'
 
-export interface Square {
-    square: string,
+
+/**
+ * A place is a square with a piece on it.
+ */
+export interface Place {
+    square: Square,
     type: PieceSymbol,
     color: Color,
 }
@@ -18,11 +22,11 @@ const PIECE_VALUES: PieceValues = {
     k: 0,
 }
 
-export function occupiedSquares(b: Chess, color: Color): Square[] {
-    let result: Square[] = []
-    for (let s of _.flatten(b.board())) {
-        if (s && s.color === color) {
-            result.push(s)
+export function places(b: Chess, color: Color): Place[] {
+    let result: Place[] = []
+    for (let pl of _.flatten(b.board())) {
+        if (pl && pl.color === color) {
+            result.push(pl)
         }
     }
     return result
@@ -30,8 +34,89 @@ export function occupiedSquares(b: Chess, color: Color): Square[] {
 
 export function material(b: Chess, color: Color): number {
     let result = 0
-    for (let s of occupiedSquares(b, color)) {
-        result += PIECE_VALUES[s.type]
+    for (let pl of places(b, color)) {
+        result += PIECE_VALUES[pl.type]
     }
     return result
+}
+
+/**
+ * A `Delta` is a relative co-ordinate reference from a square: [file, rank]
+ */
+type Delta = [number, number]
+
+const fileNumber = (char: string) => char.charCodeAt(0) - 'a'.charCodeAt(0)
+const rankNumber = (char: string) => char.charCodeAt(0) - '1'.charCodeAt(0)
+const fileChar = (num: number) => String.fromCharCode(num + 'a'.charCodeAt(0))
+const rankChar = (num: number) => String.fromCharCode(num + '1'.charCodeAt(0))
+
+/**
+ * Add a list of deltas to a square and return the resulting squares.
+ * @param sq 
+ */
+export function deltaSet(sq: Square, deltas: Delta[]): Square[] {
+    let result: Square[] = []
+    for (let d of deltas) {
+        let [file, rank] = sq.split('')
+        let [df, dr] = d
+        let f = fileNumber(file) + df
+        let r = rankNumber(rank) + dr
+        if (f >= 0 && f <= 7 && r >= 0 && r <= 7) {
+            result.push(`${fileChar(f)}${rankChar(r)}` as Square)
+        }
+    }
+    return result
+}
+
+/**
+ * Return the squares that are attacked by a pawn.
+ */
+export function pawnAttacks(sq: Square, color: Color): Square[] {
+    let deltas: Delta[] = []
+    if (color === 'w') {
+        deltas = [[-1, 1], [1, 1]]
+    } else {
+        deltas = [[-1, -1], [1, -1]]
+    }
+    return deltaSet(sq, deltas)
+}
+
+/**
+ * Return the squares that are attacked by a knight.
+ */
+export function knightAttacks(sq: Square): Square[] {
+    let deltas: Delta[] = [
+        [-1, 2], [-1, -2], [1, 2], [1, -2],
+        [-2, 1], [-2, -1], [2, 1], [2, -1],
+    ]
+    return deltaSet(sq, deltas)
+}
+
+/**
+ * Return the squares that are attacked by a bishop.
+ */
+export function bishopAttacks(sq: Square): Square[] {
+    let deltas: Delta[] = []
+    for (let i = 1; i <= 7; i++) {
+        deltas.push([i, i], [-i, i], [i, -i], [-i, -i])
+    }
+    return deltaSet(sq, deltas)
+}
+
+/**
+ * Return the squares that are attacked by a rook.
+ */
+export function rookAttacks(sq: Square): Square[] {
+    let deltas: Delta[] = []
+    for (let i = 1; i <= 7; i++) {
+        deltas.push([i, 0], [-i, 0], [0, i], [0, -i])
+    }
+    return deltaSet(sq, deltas)
+}
+
+/**
+ * Return the squares that are attacked by a queen.
+ */
+export function queenAttacks(sq: Square): Square[] {
+    return _.union(bishopAttacks(sq), rookAttacks(sq))
 }
